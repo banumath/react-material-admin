278664c1d777a162b8bbb1979b9c740fb6376bca
Dulaj Ariyaratne <dulajdeshans@gmail.com>
2020-02-04 08:32:05 +0530

    Updates

--- sw.js
+++ sw.js
@@ -55,7 +55,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "2/4/2020, 8:25:31 AM",
+  "version": "2/4/2020, 8:28:58 AM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
